ddfbb2fe1a5bb6bf8c6e545bc5e65b6add4e5bde
Burhan Uddin <whoisburhan@gmail.com>
2024-01-30 10:11:37 +0600

    Add new variant Battle Unit

--- Assets/MyWork/Scripts/Data/SaveState.cs
+++ Assets/MyWork/Scripts/Data/SaveState.cs
@@ -7,7 +7,7 @@ namespace WeAreFighters3D.Data
         public int BaseHealthIndex = 0;
         public int EvolutionIndex = 0;
         public int EnemyEvolutionIndex = 0;
-        public int CurrentTireUnlockedBattleUnitIndex = 0;
-        public int CoinAmount = 2000;
+        public int CurrentTireUnlockedBattleUnitIndex = 1;
+        public int CoinAmount = 50;
     }
 }
